324075704e5ea560686c985eef00d8f367c8f6de
dishagoyal1624-hub <dishagoyal1624@gmail.com>
2026-08-23 16:28:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.66%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num >= 10:
+            digit_sum = 0
+            while num > 0:
+               digit = num % 10
+               digit_sum += digit
+               num //= 10
+            num = digit_sum
+        return num
